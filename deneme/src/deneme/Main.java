2cef5588ca9ebfa10f7352d1ec7488a0257b926f
srknhalac <srknhalac@gmail.com>
2023-08-10 12:03:08 +0300

    4

--- deneme/src/deneme/Main.java
+++ deneme/src/deneme/Main.java
@@ -3,11 +3,7 @@ package deneme;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("");
-	    System.out.println();
-	    System.out.println();
-	     System.out.println();
-	     
+	// TODO Auto-generated method stub
 
     }
 
